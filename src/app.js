50f2ab41197c275062d46e6cc8b1f46528199764
IamAKS26 <vu1f2324045@pvppcoe.ac.in>
2026-04-05 16:55:11 +0530

    adding url

--- src/app.js
+++ src/app.js
@@ -44,10 +44,7 @@ app.use(helmet());
 const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:5000",
-  "http://localhost:3001",
-  "http://127.0.0.1:3000",
-  "http://127.0.0.1:5000",
-  "https://your-frontend-domain.com"
+  "https://ed-ustream.vercel.app/"
 ];
 
 app.use(

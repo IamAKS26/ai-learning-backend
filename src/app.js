3659b6db0bdc2b4cbde472c97cf83eff8ff410e7
IamAKS26 <vu1f2324045@pvppcoe.ac.in>
2026-04-05 17:02:06 +0530

    last depoyment fix

--- src/app.js
+++ src/app.js
@@ -92,13 +92,5 @@ setupSwagger(app);
 // ── Error handler (must be last) ───────────────────────────────────────────
 app.use(errorHandler);
 
-// ── Start server (Ignore port listener on Vercel) ──
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
-
 // ── Export for Serverless Deployment (Vercel) ──
 export default app;
